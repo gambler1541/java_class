c70c3da95f2df4a0c8c016d73bb795929b2e9921
parkmyengbae <gambler1541@gmail.com>
2020-03-20 13:21:35 +0900

    Relational Operator example

---
+++ java_320/Arithmetic_10.java
@@ -0,0 +1,33 @@
+public class Arithmetic_10 
+{
+	public static void main(String[] args) 
+	{
+		char c = 'a';
+
+		for (int i = 0; i < 26 ; i++)
+		{
+			System.out.print(c++);
+		}
+
+		System.out.println();
+
+		c = 'A';
+
+		for (int i = 0; i < 26 ; i++ )
+		{
+			System.out.print(c++);
+		}
+		
+		System.out.println();
+
+		c = 48;
+
+		for (int i = 0; i < 10 ; i ++ )
+		{
+			System.out.print(c++);
+		}
+
+		System.out.println();
+
+	}
+}
