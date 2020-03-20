58d70ec2c405cc524f1736ba40532621b98294d1
parkmyengbae <gambler1541@gmail.com>
2020-03-20 09:34:01 +0900

    'Precision'

---
+++ java_320/Precision_1.java
@@ -0,0 +1,18 @@
+public class Precision_1 
+{
+	public static void main(String[] args) 
+	{
+		float ff_1 = 1.234567890123456789f;
+		double dd_1 = 1.234567890123456789;
+
+		float ff_2 = 0.100000001f;
+		double dd_2 = 0.100000001;
+		double dd_3 = 0.10000000000001;
+
+		System.out.println(ff_1);
+		System.out.println(dd_1);
+		System.out.println(ff_2);
+		System.out.println(dd_2);
+		System.out.println(dd_3);
+	}
+}
