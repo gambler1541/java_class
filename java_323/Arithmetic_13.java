05aa2462e76b04e02a52e282069dcb89b61c5bad
parkmyengbae <gambler1541@gmail.com>
2020-03-23 09:22:16 +0900

    boolean

---
+++ java_323/Arithmetic_13.java
@@ -0,0 +1,16 @@
+public class Arithmetic_13
+{
+	public static void main(String[] args) 
+	{
+		boolean a;
+		boolean b;
+		boolean c;
+
+		a = 10 > 3;
+		b = 10 == 3;
+		c = 10 != 3;
+		System.out.println(" a = "+ a +" b = "+ b + " c = " + c);
+
+
+	}
+}
