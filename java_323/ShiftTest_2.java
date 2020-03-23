2daed5bd357c584f8fb41b6e589f259eb72dadde
parkmyengbae <gambler1541@gmail.com>
2020-03-23 10:46:21 +0900

    bitwise operator

---
+++ java_323/ShiftTest_2.java
@@ -0,0 +1,58 @@
+public class ShiftTest_2 
+{
+	public static void main(String[] args) 
+	{
+		
+		int temp;
+		
+		System.out.println();
+		System.out.println(-8);
+		System.out.println(Integer.toBinaryString(-8));
+		System.out.println();
+
+		temp = -8 << 1;
+
+		System.out.println(" -8 << 1 " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+
+		temp = -8 << 2;
+
+		System.out.println(" -8 << 2 " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+
+		System.out.println();
+		System.out.println(-8);
+		System.out.println(Integer.toBinaryString(-8));
+		System.out.println();
+
+		temp = -8 >> 1;
+
+		System.out.println(" -8 >> 1 " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+
+		
+		temp = -8 >> 2;
+
+		System.out.println(" -8 >> 2 " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+		
+		System.out.println();
+		System.out.println(-1);
+		System.out.println(Integer.toBinaryString(-1));
+		System.out.println();
+
+		temp = -1 >>> 1;
+		System.out.println("-1 >>> 1= " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+
+		temp = -8 >>> 2;
+		System.out.println("-8 >>> 2= " + temp);
+		System.out.println(Integer.toBinaryString(temp));
+		System.out.println();
+	}
+}
