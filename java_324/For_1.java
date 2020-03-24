b3d8f36af456992600ef86bd39229f60853937ef
parkmyengbae <gambler1541@gmail.com>
2020-03-24 14:10:43 +0900

    For example

---
+++ java_324/For_1.java
@@ -0,0 +1,14 @@
+public class For_1 
+{
+	public static void main(String[] args) 
+	{
+		int cnt, sum = 0;
+
+		for (cnt = 1; cnt <=100 ; cnt ++ )
+		{
+			sum += cnt;
+		}
+
+		System.out.println("1 + 2 + 3 + ~ + 100 = " + sum);
+	}
+}
