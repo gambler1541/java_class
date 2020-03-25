ec56db26e6098e83fdae18f153a3e97ef5c0d7c3
parkmyengbae <gambler1541@gmail.com>
2020-03-25 11:09:08 +0900

    While, odd, even

--- java_324/While_1.java
+++ java_324/While_1.java
@@ -2,16 +2,25 @@ public class While_1
 {
 	public static void main(String[] args) 
 	{
-		int cnt = 0, sum = 0;
+		int cnt = 0, sum = 0, odd = 0, even = 0;
 		
 
 		// while문은 몇번을 돌지 불분명함
 		while (cnt <= 10)
 		{
-			sum += cnt;			
-			cnt++;
-		}
+			sum += cnt;
 
-		System.out.println("1 ~ 10 까지의 합은? > " + sum);
+			if (cnt % 2 == 0)
+			{
+				even += cnt;
+			}
+			else{
+				odd += cnt;
+			}
+			cnt ++;
+		}
+		System.out.println("1 ~ 10 까지의 합은?> " + sum);
+		System.out.println("1 ~ 10 까지의 홀수 합은?> " + odd);
+		System.out.println("1 ~ 10 까지의 짝수 합은?> " + even);
 	}
 }
