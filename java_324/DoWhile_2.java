0a2664c22aee3fb041ebb190b258700aed17ea76
parkmyengbae <gambler1541@gmail.com>
2020-03-25 14:43:11 +0900

    IOException

---
+++ java_324/DoWhile_2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class DoWhile_2 
+{
+	public static void main(String[] args) throws IOException
+	{
+		int input = 0;
+
+		System.out.println("문장을 입력하세요 !!");
+		System.out.println("문장을 마치려면 x 를 누르세요 !!");
+		
+
+		do
+		{
+			input = System.in.read();
+			System.out.print((char)input);
+		}
+		while (input !='x');
+
+	}
+}
