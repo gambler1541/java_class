825a5505a63c7d8923cb3d373abf20162d169c78
parkmyengbae <gambler1541@gmail.com>
2020-03-27 08:36:11 +0900

    로또 당첨번호

---
+++ java_327/Lotto_1.java
@@ -0,0 +1,31 @@
+public class Lotto_1 
+{
+	public static void main(String[] args) 
+	{
+		
+		int[] number =new int[45];
+
+		int j;
+		int tmp;
+
+
+		for (int i = 0; i <= 44 ; i++ )
+		{
+			number[i] = i + 1;
+		}
+
+		for (int i =0; i < 100;i++)
+		{
+			j = (int)((44-1+1) * Math.random() + 1);
+			tmp = number[0];
+			number[0] = number[j];
+			number[j] = tmp;
+		}
+
+		for (int i =1; i <= 6; i++ )
+		{
+			System.out.printf("%d\t",number[i]);
+		}
+		System.out.println();
+	}
+}
