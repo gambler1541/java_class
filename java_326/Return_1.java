19051b550d3576fbc75c5c1f6c23b73d84be208e
parkmyengbae <gambler1541@gmail.com>
2020-03-26 10:40:15 +0900

    return받는 메서드, 받지 않는 메서드

--- java_326/Return_1.java
+++ java_326/Return_1.java
@@ -2,28 +2,29 @@ public class Return_1
 {
 	public static void main(String[] args) 
 	{
-		int result1, result2, result3, result4;
+		int result3, result4;
 
-		result1 = sum(10, 20);
-		result2 = sub(10, 20);
-		result3 = mul(10,20);
+		result3 = mul(10,20); // return값이 있는 메서드(result3,4에 저장)
 		result4 = div(40 , 4);
-
-		System.out.println("10 + 20 = " + result1);
-		System.out.println("10 - 20 = " + result2);
+		
+		sum(10,20);  // return되지 않는 메서드
+		sub(10,20);
+	
 		System.out.println("10 * 20 = " + result3);
 		System.out.println("40 / 4 = " + result4);
 
 	}
 
-	static int sum(int x, int y) // static : 이미 만들어진 method (로드 시 만들어져 있어야 함)
+	static void sum(int x, int y) // static : 이미 만들어진 method (로드 시 만들어져 있어야 함)
 	{
-		return (x + y);
+		System.out.println("10 + 20 = " + (x + y));
+		return;							// void에서는 return문을 생략할수 있다.
 	}
 
-	static int sub(int x, int y)
+	static void sub(int x, int y) // return 받지않는 메서드는 자료형이 아닌 void
 	{
-		return (x - y);
+		System.out.println("10 - 20 = " + (x - y));
+		return;
 	}
 
 	static int mul(int x, int y)
