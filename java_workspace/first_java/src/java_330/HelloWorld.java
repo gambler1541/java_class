52a5be6e169b834f5768782c2a35f09d4796cb2d
parkmyengbae <gambler1541@gmail.com>
2020-03-30 10:22:24 +0900

    eclipse

---
+++ java_workspace/first_java/src/java_330/HelloWorld.java
@@ -0,0 +1,14 @@
+package java_330;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("korea");
+		System.out.println("korea");
+		System.out.println("korea");
+		System.out.println("korea");
+		System.out.println("korea");
+
+	}
+
+}
---
+++ java_workspace/first_java/src/java_330/Text_1.java
@@ -0,0 +1,12 @@
+package java_330;
+
+public class Text_1 {
+	public static void main(String[] args) {
+		  System.out.println("아름다운 대한민국"); 
+		  System.out.println("아름다운 대한민국");
+		  System.out.println("아름다운 대한민국");
+		  System.out.println("아름다운 대한민국");
+		  System.out.println("아름다운 대한민국");
+		 	}
+
+}
