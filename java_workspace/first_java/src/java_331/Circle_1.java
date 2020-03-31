f4a8be7a419f5513609fba1c12f9c38a97d460df
parkmyengbae <gambler1541@gmail.com>
2020-03-31 13:15:59 +0900

    final, 상수

---
+++ java_workspace/first_java/src/java_331/Circle_1.java
@@ -0,0 +1,17 @@
+package java_331;
+
+
+
+public class Circle_1 {
+	public static void main(String[] args) {
+		
+		final float PI = 3.1415f;	// 상수는 대문자 쓰기를 권함
+		int r = 10;
+		
+		r = 20;
+		
+		double area = PI * r * r;
+		
+		System.out.println("원의 넓이: " + area);
+	}
+}
--- java_workspace/first_java/src/java_331/StaticRun.java
+++ java_workspace/first_java/src/java_331/StaticRun.java
@@ -3,26 +3,27 @@ package java_331;
 
 class Static_1{
 	
-	static int num = 0;
+	static int num = 0;		// static으로 만들어진 멤버(변수)는 클래스명으로 접근 가능
+	static int num_2 = 10;
 	int a = 10;
 	int b = 20;
 	
 }
 
 
-
 public class StaticRun {
 	public static void main(String[] args) {
-	
 		Static_1 ss1 = new Static_1();
 		Static_1 ss2 = new Static_1();
 		
-		ss1.num = 77;
+		Static_1.num = 77;
+		ss1.num_2 = 88;
 		ss1.a = 33;
 		ss1.b = 44;
 		
-		System.out.println("ss1 a: "+ss1.a+"  ss1 b: "+ss1.b+"  ss1.num: "+ss1.num);
-		System.out.println("ss2 a: "+ss2.a+"  ss2 b: "+ss2.b+"  ss2.num: "+ss2.num);
+		System.out.println("ss1 a: "+ss1.a+"  ss1 b: "+ss1.b+"  ss1.num: "+ss1.num_2);
+		System.out.println("ss2 a: "+ss2.a+"  ss2 b: "+ss2.b+"  ss2.num: "+ss2.num_2);
 		System.out.println("Static num: " + Static_1.num);
+		System.out.println("Static num: " + Static_1.num_2);
 	}
 }
