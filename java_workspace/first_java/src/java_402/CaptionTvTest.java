7f70a805421ce08c6bc87aa06df863ead314a277
parkmyengbae <gambler1541@gmail.com>
2020-04-02 15:38:38 +0900

    상속

---
+++ java_workspace/first_java/src/java_402/CaptionTvTest.java
@@ -0,0 +1,52 @@
+package java_402;
+
+class Tv{
+	
+	boolean power;	//  전원
+	int channel;	//  채널
+	
+	
+	void power() { power = !power; }
+
+	void channelUp() { channel++; }
+	
+	void channelDown() { -- channel; } 
+
+}
+
+class CaptionTv extends Tv{
+	
+	boolean caption; // 캡션 on & off
+	
+	void displayCaption(String text) {
+		
+		if (caption) {
+			System.out.println(" text = " + text);
+		}
+	}
+}
+
+
+public class CaptionTvTest {
+	public static void main(String[] args) {
+		CaptionTv ctv = new CaptionTv();
+		
+		ctv.channel = 11;
+		ctv.channelUp();
+		ctv.channelUp();
+		
+		ctv.caption=true;	
+		System.out.println(ctv.channel);
+		
+		ctv.displayCaption("Hello KBS ~~~");
+		
+		ctv.displayCaption("Hello CNN @@@");
+
+		ctv.channelDown();
+		ctv.channelDown();
+		ctv.channelDown();
+		ctv.channelDown();
+		ctv.channelDown();
+		System.out.println(ctv.channel);
+	}
+}
