44611c9deb9bb43a9b096508b63d7400dcdd1f28
parkmyengbae <gambler1541@gmail.com>
2020-04-06 10:48:18 +0900

    Exception example

---
+++ java_workspace/first_java/src/java_406/Exception_1.java
@@ -0,0 +1,46 @@
+package java_406;
+
+import java.io.*;		// java.io 내에 있는 모든 클래스를 import(*)
+
+public class Exception_1 {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (true) {
+			
+			try {
+				System.out.println("첫번째 값을 입력하세요!!> ");
+				int num1 = Integer.parseInt(input.readLine());
+				
+				System.out.println("두번째 값을 입력하세요!!> ");
+				int num2 = Integer.parseInt(input.readLine());
+				
+				System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+		
+			}
+
+			catch (NumberFormatException e2) {
+				System.out.println(" 입력값이 숫자여야 합니다.");
+				
+				e2.printStackTrace();
+				System.out.println(e2.getMessage());
+			}
+			
+			catch (ArithmeticException e) {
+				System.out.println(" 분모에 0 이 올수 없습니다. ");
+				
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+			
+			
+			
+			finally {	// error와 상관없이 항시 나옴
+				System.out.println("항시 나온다~");
+			}
+			
+			
+		}
+	}
+}
