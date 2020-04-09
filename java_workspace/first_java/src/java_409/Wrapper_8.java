d978b0437e35d634408caea188ee617a49b27f8a
parkmyengbae <gambler1541@gmail.com>
2020-04-09 13:44:29 +0900

    Byte Wrapper class

--- java_workspace/first_java/src/java_409/Wrapper_8.java
+++ java_workspace/first_java/src/java_409/Wrapper_8.java
@@ -1,5 +1,35 @@
 package java_409;
 
 public class Wrapper_8 {
-
+	public static void main(String[] args) {
+		
+		int bb_data = 0;
+		
+		Byte bb = new Byte("111");
+		
+		bb_data = bb.byteValue() + Byte.MAX_VALUE;
+		
+		Byte bb_2 = (byte)(bb.byteValue() + Byte.MAX_VALUE);
+		
+		System.out.println("bb.byteValue() + Byte.MAX_VALUE = " + bb_data);
+		System.out.println("(byte)(bb.byteValue() + Byte.MAX_VALUE) = " + bb_2);
+		
+		
+		byte bbb = 0;
+		int ii = 0;
+		
+		for (int i = 0; i < 300; i++) {
+			
+			System.out.println("byte : " + ++bbb + ", int : " +  ++ii );
+		}
+		
+		Byte bbb_2 = new Byte("0");
+		int ii_2 = 0;
+		
+		for (int i = 0; i < 300; i++) {
+			System.out.println("BByte : " + ++bbb_2 + ", int_2 : " + ++ii_2);
+			
+			
+		}
+	}
 }
