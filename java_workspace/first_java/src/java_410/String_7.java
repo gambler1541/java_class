66fb6a359b369edecddae401c5d3a3917664eec7
parkmyengbae <gambler1541@gmail.com>
2020-04-10 13:03:41 +0900

    오탈자 수정

--- java_workspace/first_java/src/java_410/String_7.java
+++ java_workspace/first_java/src/java_410/String_7.java
@@ -2,25 +2,14 @@ package java_410;
 
 public class String_7 {
 	public static void main(String[] args) {
-		
 		String str = "	몇 개의 단어가 포함되어 있나요!";
-		
 		int count = 0;
-		
 		System.out.println(str.length());
-		
 		for (int i = 0; i < str.length(); i++) {
-			
 			if (str.charAt(i) == 32) {			// 32 = ASCII code 공백
-				
 				count ++;
 			}
-			
 		}
-		
-		
 		System.out.println("단어의 갯수 : " + (count + 1) + " 개");
-		
-	
 	}
 }
