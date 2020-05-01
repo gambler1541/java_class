ab46a4edaa8274ee8747f51ff666956f11df61e8
parkmyengbae <gambler1541@gmail.com>
2020-05-01 15:43:21 +0900

    File input / output

--- java_workspace/first_java/src/java_501/File_1.java
+++ java_workspace/first_java/src/java_501/File_1.java
@@ -1,6 +1,7 @@
 package java_501;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class File_1 {
@@ -8,29 +9,30 @@ public class File_1 {
 	static int totalFiles = 0;
 	static int totalDirs = 0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 
-		if (args.length != 1) {
+		if (args.length != 1) { // 매개변수가 1이 아니라면(매개변수가 한개일때만 프로그램이 동작)
 			System.out.println("Usage : Java File_1 directory");
 			System.exit(0);
 		}
 
-		File dir = new File(args[0]);
+		File dir = new File(args[0]); // 매개변수의 첫번째(0번 index)
 
 		if (!dir.exists() || !dir.isDirectory()) {
 			System.out.println("유효하지않는 디렉토리입니다.");
 			System.exit(1);
 		}
-		printFileList(dir);
+		
+		printFileList(dir); // File객체가 넘어감
 		
 		System.out.println();
 		System.out.println("총 : " + totalFiles + " 개의 파일");
 		System.out.println("총 : " + totalDirs + " 개의 디렉토리");
 	}
 
-	static void printFileList(File dir) {
-		System.out.println(dir.getAbsolutePath() + "디렉토리");
-		File[] files = dir.listFiles();
+	static void printFileList(File dir) throws IOException {
+		System.out.println(dir.getAbsolutePath() + "디렉토리"); //getAbsolutePath() 절대경로를 보여줌
+		File[] files = dir.listFiles();	//
 		
 		ArrayList subDir = new ArrayList();
 		
